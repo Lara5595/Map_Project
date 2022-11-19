339a310f37fe5853964bf8efc73de5c807f17912
David Lara <david.a.lara05@gmail.com>
2022-11-19 17:35:40 -0600

    finish map

--- js/weather_map.js
+++ js/weather_map.js
@@ -84,7 +84,7 @@ $(function () {
             if(i % 8 == 0) {
                 $(`#cards`).append(`<div class="card-header col-2 mx-2 border"> <p class="date">${data.list[i].dt_txt.split(" ")[0]}</p>
                  <p class="temp"><img src="http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png"><br> ${data.list[i].main.temp}&#8457 / ${data.list[i].main.temp}&#8457;</p><hr>
-                 <p class="city">City: ${data.city.name}</p><hr>
+                 <p class="city">Location: ${data.city.name}</p><hr>
                  <p class="description">Description: ${data.list[i].weather[0].description}</p><hr>
                  <p class="humidity">Humidity: ${data.list[i].main.humidity}</p><hr>
                  <p class="wind">Wind: ${data.list[i].wind.speed}</p><hr>
@@ -102,7 +102,7 @@ $(function () {
             if(i % 8 == 0) {
                 $(`#cards`).append(`<div class="card-header col-2 mx-2 border"> <p class="date">${data.list[i].dt_txt.split(" ")[0]}</p>
                  <p class="temp"><img src="http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png"><br>${data.list[i].main.temp}&#8457 / ${data.list[i].main.temp}&#8457;</p><hr>
-                 <p class="city">City: ${data.city.name}</p><hr>
+                 <p class="city">Location: ${data.city.name}</p><hr>
                  <p class="description">Description: ${data.list[i].weather[0].description}</p><hr>
                  <p class="humidity">Humidity: ${data.list[i].main.humidity}</p><hr>
                  <p class="wind">Wind: ${data.list[i].wind.speed}</p><hr>
